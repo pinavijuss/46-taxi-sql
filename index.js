81df65e0f31dbd532505c25b6d2f0dcbf1207bc6
pinavijuss <pijus.dalgeda@gmail.com>
2021-09-02 19:48:45 +0300

    task

---
+++ index.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+
+const app = {}
+
+app.init = async () => {
+    // prisijungti prie duomenu bazes
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'taxi',
+    });
+
+    let sql = '';
+    let rows = [];
+
+
+
+}
+
+app.init();
+
+module.exports = app;
